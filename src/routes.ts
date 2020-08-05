fa9afd1a01495546a00c761f6085f5c58b63ebae
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-04 22:54:20 -0300

    inserindo usuario no db

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,26 @@
-import { Router, Request, Response } from 'express'
+import { Router, Request, Response } from 'express';
+
+import db from './database/connections';
 
 const routes = Router();
 
-routes.post('/classes', async (request: Request, response: Response) => {
-  const { body } = request;
+routes.post('/classes', async (request: Request, response: Response): Promise<Response> => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule,
+  } = request.body;
 
-  console.log(body);
+  await db('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    bio,
+  });
 
   return response.json({ ok: true });
 })
