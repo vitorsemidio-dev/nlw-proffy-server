7afd6b3da7ac7fd7c3adb02427bddcfec449d687
Emidio <52754546+vitorsemidio-dev@users.noreply.github.com>
2020-08-05 00:20:08 -0300

    Total conexoes (#4)
    * Criação conexão
    
    * Total conexões
    
    * Adição cors

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = Router();
 const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
+
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
 
 export default routes
