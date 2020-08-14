00f84e796be131c09b437a862dcc9f95a5b4a451
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-13 23:32:31 -0300

    Remoção código

--- src/shared/infra/knex/migrations/06_remove_field_avatar_from_users.ts
+++ src/shared/infra/knex/migrations/06_remove_field_avatar_from_users.ts
@@ -4,16 +4,10 @@ export async function up(knex: Knex): Promise<Knex.SchemaBuilder> {
   return knex.schema.table('users', table => {
     table.dropColumn('avatar');
   });
-  // return knex.schema.alterTable('users', table => {
-  //   table.string('avatar').nullable().alter();
-  // });
 }
 
 export async function down(knex: Knex): Promise<Knex.SchemaBuilder> {
   return knex.schema.table('users', table => {
     table.string('avatar').notNullable();
   });
-  // return knex.schema.alterTable('users', table => {
-  //   table.string('avatar').notNullable().alter();
-  // });
 }
