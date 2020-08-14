6e276f0680d23e59a8e4e9cd3a9198f9668c774f
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-13 21:12:47 -0300

    Serviço de criação de matéria chamado no controlelr

--- src/modules/classes/infra/http/controllers/SubjectsController.ts
+++ src/modules/classes/infra/http/controllers/SubjectsController.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from 'express';
 
+import CreateSubjectsService from '../../../services/CreateSubjectsService';
+
+const createSubjectsService = new CreateSubjectsService();
+
 class SubjectsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    return response.json();
+    const { name } = request.body;
+
+    const subject = await createSubjectsService.execute({ name });
+
+    return response.json(subject);
   }
 
   public async index(request: Request, response: Response): Promise<Response> {
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import usersRouter from '../../../../modules/users/infra/http/routes/users.routes';
 import classesRouter from '../../../../modules/classes/infra/http/routes/classes.routes';
 import connectionsRouter from '../../../../modules/classes/infra/http/routes/connections.routes';
+import subjectsRouter from '../../../../modules/classes/infra/http/routes/subjects.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/classes', classesRouter);
+routes.use('/subjects', subjectsRouter);
 routes.use('/connections', connectionsRouter);
 
 export default routes;
