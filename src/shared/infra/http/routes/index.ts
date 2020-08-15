efea6f3a1732f776692b6c75bd2fc6edc8e748a6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-15 00:59:52 -0300

    Ajustes paths routes index

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
-import usersRouter from '../../../../modules/users/infra/http/routes/users.routes';
-import classesRouter from '../../../../modules/classes/infra/http/routes/classes.routes';
-import connectionsRouter from '../../../../modules/classes/infra/http/routes/connections.routes';
-import subjectsRouter from '../../../../modules/classes/infra/http/routes/subjects.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import classesRouter from '@modules/classes/infra/http/routes/classes.routes';
+import connectionsRouter from '@modules/classes/infra/http/routes/connections.routes';
+import subjectsRouter from '@modules/classes/infra/http/routes/subjects.routes';
 
 const routes = Router();
 
