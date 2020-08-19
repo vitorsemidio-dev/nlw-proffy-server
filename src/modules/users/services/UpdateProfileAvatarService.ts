bdf3cd02bc438548d8c96970183c26feea21bfa3
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-19 02:45:36 -0300

    Implementação UpdateProfileAvatarService

---
+++ src/modules/users/services/UpdateProfileAvatarService.ts
@@ -0,0 +1,32 @@
+import db from '@shared/infra/knex/connections';
+
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+
+interface IRequest {
+  user_id: number;
+  file: string;
+}
+
+export default class UpdateProfileAvatarService {
+  constructor(private storageProvider: IStorageProvider) {}
+
+  public async execute({ file, user_id }: IRequest): Promise<void> {
+    const user = await db
+      .select('*')
+      .from('users')
+      .where('user_id', user_id)
+      .first();
+
+    if (!user) {
+      throw new Error('User does not exist');
+    }
+
+    if (user.avatar) {
+      await this.storageProvider.deleteFile(user.avatar);
+    }
+
+    const filename = await this.storageProvider.saveFile(file);
+
+    await db('users').where('id', '=', user_id).update('avatar', filename);
+  }
+}
